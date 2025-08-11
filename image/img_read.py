2f835faf97806455ec528de35ec289ad88c82098
syugo0101 <mamo0101ru@gmail.com>
2025-08-11 14:34:14 +0900

    マジックナンバーの排除

--- image/img_read.py
+++ image/img_read.py
@@ -4,6 +4,7 @@ import cv2
 
 imgplace = "D:\\testimege\\"
 imglist = [["s1.jpg",0], ["s2.jpg",0]]
+area_threshold = 100000
 
 for i in imglist:
     path = imgplace + i[0]
@@ -11,7 +12,7 @@ for i in imglist:
     counter = count_write.CountWhiteArea(path, i[1])
     white_area, white_mask = counter.get_white_area()
     print(f"白色領域の面積(ピクセル数): {white_area}")
-    if white_area > 100000:
+    if white_area > area_threshold:
         print(i[0], "切子が多い")
     else:
         print(i[0], "切子が少ない")
